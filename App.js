8633bf8c7cb73c65cbd7229a96b15a59b5e55f1a
kartikhelps <kartik@techhelps.co.in>
2023-06-23 17:27:37 +0530

    remove crash apps

--- App.js
+++ App.js
@@ -3,58 +3,59 @@ import { FlatList, StyleSheet, Text, View } from "react-native";
 import { AppBar, IconButton, FAB } from "@react-native-material/core";
 import Data from "./Food.json";
 import Card from "./Custom/Card";
-import { SafeAreaView } from "react-native-safe-area-context";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
-// import { Animated } from "react-native-web";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const iconSize = 35;
 
 export default function App() {
   return (
-    <>
-      <AppBar title="MA foodWala" style={styles.navbar} />
-      <View style={styles.container}>
-        <View
-          style={{
-            backgroundColor: "#ffdb70",
-            padding: 10,
-            margin: 10,
-            borderRadius: 10,
-          }}
-        >
-          <Text style={{ fontSize: 40, padding: 5 }}>Our menu</Text>
-        </View>
-        <StatusBar style="auto" />
-        <SafeAreaView style={{ flex: 1, marginBottom: 60 }}>
-          <FlatList
-            data={Data}
-            renderItem={({ item }) => (
-              <Card children={item.food} rate={item.rate} image={item.image} />
-            )}
-            keyExtractor={(item) => item}
-          />
-        </SafeAreaView>
-        <AppBar variant="bottom" style={styles.bottomAppBar} color="pink">
-          <IconButton
-            icon={<Icon name="home" size={iconSize} />}
-            style={{ position: "absolute", left: 60 }}
-          />
-          <FAB
-            icon={<Icon name="cart" size={28} />}
+    <SafeAreaProvider>
+      <>
+        <AppBar title="MA foodWala" style={styles.navbar} />
+        <View style={styles.container}>
+          <View
             style={{
-              position: "absolute",
-              top: -28,
-              alignSelf: "center",
-              backgroundColor: "#9dc183",
+              backgroundColor: "#ffdb70",
+              padding: 10,
+              margin: 10,
+              borderRadius: 10,
             }}
-          />
-          <IconButton
-            icon={<Icon name="menu" size={iconSize} />}
-            style={{ position: "absolute", right: 60 }}
-          />
-        </AppBar>
-      </View>
-    </>
+          >
+            <Text style={{ fontSize: 40, padding: 5 }}>Our menu</Text>
+          </View>
+          <StatusBar style="auto" />
+          <SafeAreaView style={{ flex: 1, marginBottom: 60 }}>
+            <FlatList
+              data={Data}
+              renderItem={({ item }) => (
+                <Card children={item.food} rate={item.rate} image={item.image} />
+              )}
+              keyExtractor={(item) => item}
+            />
+          </SafeAreaView>
+          <AppBar variant="bottom" style={styles.bottomAppBar} color="pink">
+            <IconButton
+              icon={<Icon name="home" size={iconSize} />}
+              style={{ position: "absolute", left: 60 }}
+            />
+            <FAB
+              icon={<Icon name="cart" size={28} />}
+              style={{
+                position: "absolute",
+                top: -28,
+                alignSelf: "center",
+                backgroundColor: "#9dc183",
+              }}
+            />
+            <IconButton
+              icon={<Icon name="menu" size={iconSize} />}
+              style={{ position: "absolute", right: 60 }}
+            />
+          </AppBar>
+        </View>
+      </>
+    </SafeAreaProvider>
   );
 }
 
